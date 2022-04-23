24d96acba260a9b5573ddf44944a295db808c875
Presiareen <qiu.ke@qq.com>
2022-04-24 01:04:26 +0800

    docs: add toc

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,12 +1,24 @@
-import { defineUserConfig } from 'vuepress';
+import { defineUserConfig } from 'vuepress'
+import type { DefaultThemeOptions } from 'vuepress'
 
-defineUserConfig({
+export default defineUserConfig<DefaultThemeOptions>({
     base: "/",
-    locales: {
-        '/': {
-            lang: "ZH-cn",
-            title: "服务器管理文档",
-            description: ""
-        } 
+    lang: "ZH-cn",
+    title: "服务器管理文档",
+    description: "",
+    themeConfig: {
+        sidebar: [{
+            text: "指南",
+            children: [
+                "/README.md",
+                {
+                    text: "Web 服务",
+                    children: ["/web/README.md"]
+                }, {
+                    text: "PBS 队列服务",
+                    children: ["/pbs/README.md"]
+                }
+            ]
+        }]
     }
 });
--- docs/README.md
+++ docs/README.md
@@ -1,5 +1,5 @@
 ---
-title: 123
+title: 须知
 ---
 # Hello World!
 
